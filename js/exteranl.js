26486f0ca1ba3559d4c463335108993c6c753807
Amit <amitjhar1999@gmail.com>
2022-05-11 11:54:48 +0530

    commit

--- js/exteranl.js
+++ js/exteranl.js
@@ -1,1 +1,30 @@
 console.log("amit jangid")
+
+
+
+
+
+
+
+
+var index = 30;
+if(index % 2 == 1){
+    console.log("odd number")
+}else{
+    console.log("even number")
+}
+
+var index = 1;
+while(index<15){
+   if(index % 2 ==1){
+       console.log('odd ')
+       
+   }
+   else{
+       console.log('even')
+   }
+   index=index+1
+    
+}
+  
+
